687045061e82ae25a04e70857f7e701520864553
UDAY-518 <111357317+Udaybabubojja@users.noreply.github.com>
2024-06-01 14:29:34 +0530

    Time: 1 ms (99.87%) | Memory: 41.9 MB (60.00%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -1,8 +1,9 @@
 class Solution {
     public int scoreOfString(String s) {
         int score=0;
+        StringBuilder sb = new StringBuilder(s);
         for(int i=0; i<s.length()-1; i++){
-            score+=Math.abs(s.charAt(i)-s.charAt(i+1));
+            score+=Math.abs(sb.charAt(i)-sb.charAt(i+1));
         }
         return score;
     }
