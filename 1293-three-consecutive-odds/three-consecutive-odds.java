3002822bbca27c662bb7fae48017a5ba05f3a8a1
UDAY-518 <111357317+Udaybabubojja@users.noreply.github.com>
2024-07-01 12:39:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (99.76%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        if(arr.length<3) return false;
+        for(int i=0; i<arr.length-2; i++){
+            if(((arr[i]&1)==1)&&((arr[i+1]&1)==1)&&((arr[i+2]&1)==1)) return true;
+        }
+        return false;
+    }
+}
